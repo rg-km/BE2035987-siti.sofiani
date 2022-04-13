e8e2c7f941261f8da282634e0120a015a3cb0f1c
Cupii18 <ekamayanauranabila@gmail.com>
2022-04-13 14:20:05 +0700

    backend

--- backend/data-structure/assignment/scoreboard/main.go
+++ backend/data-structure/assignment/scoreboard/main.go
@@ -18,14 +18,15 @@ func (s Scores) Len() int {
 }
 
 func (s Scores) Less(i, j int) bool {
-	score1 := s[i].Correct * 4 - s[i].Wrong
-	score2 := s[j].Correct * 4 - s[j].Wrong
+	score1 := s[i].Correct*4 - s[i].Wrong
+	score2 := s[j].Correct*4 - s[j].Wrong
 
 	if scoreI > scoreJ {
 		return true
 	} else scoreI < scoreJ {
 		return false
-	}else{
+	} else {
+		
 		if s[i].Correct > s[j].Correct {
 			return true
 		} else if s[i].Correct < s[j].Correct {
