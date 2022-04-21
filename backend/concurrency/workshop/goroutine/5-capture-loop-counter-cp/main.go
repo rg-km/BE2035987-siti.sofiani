9db0d53b2f186bff8d68241bb246e8b9aa11f8bb
Cupii18 <ekamayanauranabila@gmail.com>
2022-04-21 12:23:33 +0700

    bbb

--- backend/concurrency/workshop/goroutine/5-capture-loop-counter-cp/main.go
+++ backend/concurrency/workshop/goroutine/5-capture-loop-counter-cp/main.go
@@ -1,18 +1,37 @@
-package main
+package main_test
 
 import (
 	"fmt"
 	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-func main() {
-	go func() {
-		result := 0
-		for i := 0; i < 5; i++ {
-			result += i
-		}
-		time.Sleep(10 * time.Millisecond)
-		fmt.Println("main stop")
-	}()
+var _ = Describe("Goroutine", func() {
+	When("used with anonymous function", func() {
+		It("can capture loop counter correctly", func() {
+			result := 0
+			for i := 0; i < 5; i++ {
+				//jalankan goroutine yang melakukan print untuk tiap hasil perkalian i*i dalam loop
+				//dan menambahkan hasil tersebut ke variable result
+				// TODO: answer here
+				go func(i int) {
+					fmt.Println(i * i)
+					result += i * i
+				}(i)
+			}
+			time.Sleep(10 * time.Millisecond)
+			fmt.Println("main stop")
+			Expect(result).To(Equal(30))
+		})
+	})
+})
 
-}
+//pada output terdapat angka berikut:
+// 4
+// 0
+// 16
+// 1
+// 9
+// urutan tidak harus sesuai
