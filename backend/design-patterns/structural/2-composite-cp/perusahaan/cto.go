bf2142ac57c7ebbb9aa4496e8101b90ac86200e1
Cupii18 <ekamayanauranabila@gmail.com>
2022-05-26 20:28:45 +0700

    nnn

--- backend/design-patterns/structural/2-composite-cp/perusahaan/cto.go
+++ backend/design-patterns/structural/2-composite-cp/perusahaan/cto.go
@@ -1,10 +1,5 @@
 package perusahaan
 
-<<<<<<< HEAD
-=======
-import "fmt"
-
->>>>>>> fc12154791502702980a046e3507ab317e48f675
 type CTO struct {
 	Subordinate []Employee
 }
@@ -14,14 +9,9 @@ func (c CTO) GetSalary() int {
 }
 
 func (c CTO) TotalDivisonSalary() int {
-<<<<<<< HEAD
 	total := c.GetSalary()
 	for _, employee := range c.Subordinate {
 		total += employee.TotalDivisonSalary()
 	}
 	return total
 }
-=======
-	return 0 // TODO: replace this
-}
->>>>>>> fc12154791502702980a046e3507ab317e48f675
